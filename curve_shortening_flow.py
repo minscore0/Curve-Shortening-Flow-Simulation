6cd14d3ae7b45b9b5672b0705bf756a44221421b
matthewinscore <matthewinscore@gmail.com>
2024-05-27 15:15:21 -0700

    initial pygame setup

--- curve_shortening_flow.py
+++ curve_shortening_flow.py
@@ -1,5 +1,45 @@
-import matplotlib as plt
-import numpy as np
+import pygame
 
+# pygame setup
+pygame.init()
+pygame.display.set_caption("Curve Shortening Flow Simulation")
+screen = pygame.display.set_mode((1500, 900), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+screen.fill("black")
 
+# constants
 
+def update_display(screen):
+    pygame.display.flip()
+    return
+
+# variables
+started = False
+running = True
+
+while running:
+    
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and not started:
+            pass
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q: # quit program
+                running = False
+
+            elif event.key == pygame.K_DELETE or event.key == pygame.K_c:
+                pass
+            
+            elif (event.key == pygame.K_RETURN or event.key == pygame.K_s) and not started: # start simulation
+                pass
+
+            elif event.key == pygame.K_t: # for testing
+                print("test started")
+
+    update_display(screen)
+pygame.quit()
